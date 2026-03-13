3423b3bd1cba7597590f98a7b0428548ad103026
hyh <hanyh000@naver.com>
2026-03-13 10:10:49 +0900

    강화학습 맵 파일 middle.world 이용

---
+++ 심화프로젝트/turtlebot3_gazebo/launch/middle1.launch.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Joep Tool, Hyungyu Kim
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    # 경로 설정
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_turtlebot3_gazebo = get_package_share_directory('turtlebot3_gazebo')
+
+    # 월드 파일 경로 설정
+    world = os.path.join(
+        pkg_turtlebot3_gazebo,
+        'worlds',
+        'middle1.world'
+    )
+
+    # 1. Gazebo 서버 실행 (물리 엔진)
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    # 2. Gazebo 클라이언트 실행 (GUI 창)
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    # 런치 설명서 생성
+    ld = LaunchDescription()
+
+    # 터틀봇 관련 액션들을 제외하고 가제보 실행 관련 액션만 추가
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+
+    return ld
